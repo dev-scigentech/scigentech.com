de16f1c2a205a14f4afe409b1033038de6dbab66
Aris <dev@scigentech.com>
2025-05-30 12:56:26 +0300

    fix bug

--- app/api/send-mail/route.ts
+++ app/api/send-mail/route.ts
@@ -5,8 +5,8 @@ import { SESClient, SendEmailCommand } from "@aws-sdk/client-ses";
 const sesClient = new SESClient({
   region: process.env.AWS_REGION || "us-east-1", // Replace with your AWS region
   credentials: {
-    accessKeyId: process.env.AWS_ACCESS_KEY_ID!,
-    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY!,
+    accessKeyId: process.env.AWS_ACCESS_KEY_ID || '',
+    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY || '',
   },
 });
 
